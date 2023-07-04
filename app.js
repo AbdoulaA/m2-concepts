5b9e8173c0307fa3643904b06119973c6029c5af
Abdorruhman Abdelwahed <52506803+AbdoulaA@users.noreply.github.com>
2023-07-04 00:20:15 -0500

    redesigned

--- app.js
+++ app.js
@@ -1,37 +1,111 @@
-let isPlaneVisible = false;
-function toggleHeaderOn() {
-  let home = document.getElementById("home");
+let isHeaderVisible = true;
 
-  if (isPlaneVisible) {
-    isPlaneVisible = false;
-    return home.classList.remove("hide");
-  }
-  isPlaneVisible = true;
-  home.classList += " hide";
-  console.log("plane");
-}
+function togglePlane() {
+  let plane = document.getElementById("plane");
+  let header = document.getElementById("header");
+  let bg = document.getElementById("bg__img");
 
-let isHeaderVisible = false;
-function togglePlaneOn() {
-  let plane = document.getElementById("plane__container");
   if (isHeaderVisible) {
-    isHeaderVisible = false;
-    return plane.classList.remove("hide__plane");
+    bg.classList.toggle("move__bg");
+    header.classList.toggle("hide__header");
+    return plane.classList.toggle("hide__plane");
   }
-  isHeaderVisible = true;
-  plane.classList += " hide__plane";
-  console.log("header");
+  console.log(plane);
 }
 
-// let areStoresVisible = false;
-// function toggleStoresOn() {
-//   let stores = document.getElementById("plane__stores");
-//   if (areStoresVisible) {
-//     areStoresVisible = false;
-//     return stores.classList.remove("hide__stores");
-//   }
-//   areStoresVisible = true;
-//   stores.classList += " hide__stores";
-//   console.log("stores");
-// }
+function togglePlaneOff() {
+  let plane = document.getElementById("plane");
+  let header = document.getElementById("header");
+  let bg = document.getElementById("bg__img");
 
+  bg.classList -= "move__bg";
+  header.classList -= "hide__header";
+  plane.classList -= "hide__plane";
+  isHeaderVisible = true;
+}
+
+let storesList = [
+  {
+    name: "Chick-Fil-A",
+    id: 0,
+    url: "./assets/cfa logo.png",
+  },
+  {
+    name: "Starbucks",
+    id: 1,
+    url: "./assets/starbucks logo.png",
+  },
+  {
+    name: "Coffee and Bagels",
+    id: 5,
+    url: "./assets/coffee and bagels.png",
+  },
+  {
+    name: "Qdoba Mexican Restaurant",
+    id: 3,
+    url: "./assets/qdoba logo.png",
+  },
+  {
+    name: "NewsLink",
+    id: 4,
+    url: "./assets/newslink logo.png",
+  },
+  {
+    name: "Panda Express",
+    id: 2,
+    url: "./assets/panda express logo.png",
+  },
+  {
+    name: "Blue Mediterranean Bar",
+    id: 6,
+    url: "./assets/blue logo.png",
+  },
+];
+
+function renderStores() {
+  let stores = document.getElementById("plane__stores");
+  storesHtml = ``;
+  stores.innerHTML = storesHtml;
+
+  for (i = 0; i <= storesList.length; i++) {
+    storesHtml += `<a href="" class="plane__link"></a>
+    <img src="${storesList[i].url}" alt="${storesList[i].name}" class="plane__store" id = "${i}" onclick="isoStore()" />`;
+
+    stores.innerHTML = storesHtml;
+
+    console.log("word");
+  }
+}
+
+function isoStore() {
+  const planeStores = document.querySelectorAll(".plane__store");
+
+  planeStores.forEach((store) => {
+    store.addEventListener("click", () => {
+      planeStores.forEach((s) => {
+        if (s !== store) {
+          s.classList.toggle("hide__stores");
+        }
+      });
+    });
+  });
+}
+
+// setTimeout(() => {
+//     renderStores();
+//   }, 1000);
+
+// function renderStores() {
+//   let storesWrapper = document.querySelector(".plane__stores");
+//   let stores = getStores();
+
+//   storesWrapper.innerHTML = stores
+//     .map((store) => {
+//       return `<a href="" class="plane__link"></a>
+//     <img src="${store.url}" alt="" class="plane__store" />
+//   </a>`;
+//     })
+//     .join("");
+
+//   console.log("worked");
+// }
